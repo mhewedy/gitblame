43855ba145e0f7ae133fd4c6775544053d7d1f21
Muhammad Hewedy <mhewedy@gmail.com>
2019-09-27 13:06:17 +0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitblame
+
+go 1.12
+
+require gopkg.in/src-d/go-git.v4 v4.13.1
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"log"
+)
+
+func main() {
+
+	r, err := git.PlainOpen("/Users/mhewedy/Work/Code/spring-amqp")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authors, err := getAuthors(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, author := range authors {
+		fmt.Println(author.Name, author.Email)
+	}
+}
+
+func getAuthors(r *git.Repository) ([]object.Signature, error) {
+	authors := make([]object.Signature, 0)
+
+	cIter, err := r.Log(&git.LogOptions{All: true})
+	if err != nil {
+		return nil, err
+	}
+
+	cIter.ForEach(func(c *object.Commit) error {
+		if !contains(authors, c.Author) {
+			authors = append(authors, c.Author)
+		}
+		return nil
+	})
+
+	return authors, nil
+}
+
+func contains(s []object.Signature, e object.Signature) bool {
+	for _, elem := range s {
+		if elem.Email == e.Email {
+			return true
+		}
+	}
+	return false
+}
