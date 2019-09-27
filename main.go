bd0d53e7f49d1275d6ddd996723eb28d15ccc118
Muhammad Hewedy <mhewedy@gmail.com>
2019-09-27 19:09:50 +0300

    Git commit Patch

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"io"
 	"log"
 )
 
@@ -16,7 +17,7 @@ type AuthorCommits map[Author][]object.Commit
 
 func main() {
 
-	r, err := git.PlainOpen("/Users/mhewedy/Work/Code/spring-amqp")
+	r, err := git.PlainOpen("/Users/mhewedy/Work/gobase/code/src/github.com/mhewedy/gitblame")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,22 +29,54 @@ func main() {
 
 	for author, commits := range *authors {
 		fmt.Println(author.Email, author.Name, len(commits))
-	}
+		fmt.Println("****************************")
 
-	/*ref, _ := r.Head()
-	commit, _ := r.CommitObject(ref.Hash())
-	getCommitDetails(commit)*/
+		for i, commit := range commits {
+			fmt.Println("******i=", i)
+			patch, err := getCommitDiff(&commit)
+			if err != nil {
+				log.Fatal(err, ">>>>")
+			}
+			fmt.Println(patch, "\n\n\n\n")
+		}
+
+		fmt.Println("****************************")
+	}
 }
 
-func getCommitDetails(c *object.Commit) {
-	/*
-		fmt.Println(c.Author)
-		fmt.Println(c.Committer)
-		fmt.Println(c.Hash)
-		fmt.Println(c.Message)
-		fmt.Println(c.ParentHashes)
-		fmt.Println(c.PGPSignature)
-		fmt.Println(c.TreeHash)*/
+func getCommitDiff(c *object.Commit) (*object.Patch, error) {
+
+	tree, err := c.Tree()
+	if err != nil {
+		return nil, err
+	}
+
+	parents := c.Parents()
+	defer parents.Close()
+	parent, err := parents.Next()
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	var prevTree *object.Tree
+	if parent != nil {
+		prevTree, err = parent.Tree()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	changes, err := prevTree.Diff(tree)
+	if err != nil {
+		return nil, err
+	}
+
+	patch, err := changes.Patch()
+	if err != nil {
+		return nil, err
+	}
+
+	return patch, nil
 }
 
 func groupCommitsByAuthor(r *git.Repository) (*AuthorCommits, error) {
