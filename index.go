b5eb96d75d675c9b142376a01c9abaa6152fc009
Muhammad Hewedy <mhewedy@gmail.com>
2019-09-28 11:06:59 +0300

    apply styling

--- index.go
+++ index.go
@@ -21,7 +21,12 @@ func Index(writer http.ResponseWriter, request *http.Request) {
             max-width: 1200px;
         }
     }
-
+	.card-header.collapsed {
+		background:#ccc;
+	}
+	.card-header:not(.collapsed) {
+		background:#ee6666;
+	}
 </style>
 
 <script>
